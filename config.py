8c1409eeaea133d128aa8c04a93e419caab4cb96
yanweijia <happyboyywj@163.com>
2017-12-20 16:52:27 +0800

    配置文件

---
+++ config.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+"""
+Created on 2017/11/04
+@author: weijia
+"""
+dbinfo = {
+    'host': 'localhost',
+    'port': 3306,
+    'user': 'root',
+    'pwd': 'yanweijia',
+    'db': 'words',
+    'encoding': 'utf8',
+    'mincached': 1,
+    'maxcached': 20,
+    'use_unicode': True
+}
